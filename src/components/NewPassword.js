7b831740060065793fcd860f2027962ee1a9f4e2
BintReina <omobowalerohma1998@gmail.com>
2023-05-25 14:04:28 +0100

    designed the layout

---
+++ src/components/NewPassword.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "../styles/veiledCoderStyles/newpassword.css";
+import Privacy from "../images/privacy.svg";
+import { useForm } from "react-hook-form";
+import ButtonLarge from "./ButtonLarge";
+
+function NewPassword() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+  return (
+    <div className="newpassword-container">
+      <h2>Create New Password</h2>
+      <img src={Privacy} alt="encryption illustration" />
+      <p>
+        Your password should contain{" "}
+        <span>at least 8 characters and a number</span>
+      </p>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <div className="form-group">
+          <label htmlFor="new-password">New Password</label>
+          <input
+            {...register("newPassword", {
+              required: "This is required",
+              minLength: {
+                value: 6,
+                message: "minimum password lenght is 6",
+              },
+            })}
+            id="new-password"
+            placeholder="password"
+            type="text"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            {...register("confirmPassword", { required: "This is required" })}
+            id="confirm-password"
+            placeholder="password"
+            type="text"
+          />
+        </div>
+
+        <ButtonLarge text="SAVE" />
+      </form>
+    </div>
+  );
+}
+
+export default NewPassword;
