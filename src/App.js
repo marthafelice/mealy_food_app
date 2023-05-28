72f50cf9fb91d5a372019c7e7b3c14545bedcf68
afolaode <afolaode@gmail.com>
2023-05-28 15:58:33 +0100

    completed signup form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import "./App.css"
+import "./App.css";
+import SignUp from "./components/SignUp";
 import NewPassword from "./components/NewPassword";
 function App() {
   return (
-
-    <div className="app">
-    <NewPassword/>
-
+    <div className='app'>
+      <NewPassword/>
+      {/* <SignUp /> */}
     </div>
   );
 }
