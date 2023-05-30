91c868664fc0b06c4840dc05ff27bbf1675f9e6e
BintReina <omobowalerohma1998@gmail.com>
2023-05-30 09:02:44 +0100

    built the edit name modal

---
+++ src/styles/allModals.js
@@ -0,0 +1,16 @@
+export const editprofileNameModal = {
+  overlay: {
+    background: "#6650a495",
+  },
+  content: {
+    width: "50%",
+    height: "50vh",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: " translate(-50%, -50%)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+};
