49cf2e3b72747a5130665fd59350448529fdccbb
BintReina <omobowalerohma1998@gmail.com>
2023-05-30 15:07:20 +0100

    styled the overlay

--- src/components/EditProfileName.js
+++ src/components/EditProfileName.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactModal from "react-modal";
 import { useSelector, useDispatch } from "react-redux";
 import { displayedProfileEdit } from "../redux/slices/profileSlice";
-import "../styles/editProfileName.css";
+import "../styles/editProfile.css";
 import { editprofileNameModal } from "../styles/allModals";
+import { useForm } from "react-hook-form";
+
 function EditProfileModal() {
+  const { register, handleSubmit } = useForm();
   const { displayEdit } = useSelector((state) => state.profile);
+  function editSubmit(data) {
+    console.log(data);
+  }
   const dispatch = useDispatch();
   function closeProfileEdit() {
     dispatch(displayedProfileEdit());
@@ -18,7 +24,30 @@ function EditProfileModal() {
       onRequestClose={closeProfileEdit}
       style={editprofileNameModal}
     >
-      <h2>Your Details</h2>
+      <h2 className="editProfile_heading">Your Details</h2>
+      <form className="editProfile_form" onSubmit={handleSubmit(editSubmit)}>
+        <label htmlFor="edit-name" className="editProfile_title">Name</label>
+        <input
+          className="edit-input"
+          placeholder="mariam"
+          id="edit-name"
+          {...register("edit-name", { required: "this is required" })}
+        />
+        <label htmlFor="edit-email">Name</label>
+        <input
+          className="edit-input"
+          id="edit-email"
+          placeholder="marmtee@gmail.com"
+          {...register("edit-email", {
+            required: "this is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "invalid Email Address",
+            },
+          })}
+        />
+        <button type="submit" className="edit-profile_submit">SUBMIT</button>
+      </form>
       <button onClick={closeProfileEdit}>CLOSE MODAL</button>
     </ReactModal>
   );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from 'react-redux'
+import { setAppElement } from "react-modal";
 import store from "./redux/store"
+const rootElement = document.getElementById("root");
+setAppElement(rootElement);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
