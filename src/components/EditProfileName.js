fb4698074fe02723d6f9afa3002516e2683a620d
unknown <omobowalerohma1998@gmail.com>
2023-05-31 10:07:28 +0100

    styling the edit input

--- src/components/EditProfileName.js
+++ src/components/EditProfileName.js
@@ -5,6 +5,9 @@ import { displayedProfileEdit } from "../redux/slices/profileSlice";
 import "../styles/editProfile.css";
 import { editprofileNameModal } from "../styles/allModals";
 import { useForm } from "react-hook-form";
+import editNameIcon from "../images/editname.svg";
+import editMailIcon from "../images/mail-icon.png";
+import clearEdit from "../images/clearEdit.svg";
 
 function EditProfileModal() {
   const { register, handleSubmit } = useForm();
@@ -26,27 +29,42 @@ function EditProfileModal() {
     >
       <h2 className="editProfile_heading">Your Details</h2>
       <form className="editProfile_form" onSubmit={handleSubmit(editSubmit)}>
-        <label htmlFor="edit-name" className="editProfile_title">Name</label>
-        <input
-          className="edit-input"
-          placeholder="mariam"
-          id="edit-name"
-          {...register("edit-name", { required: "this is required" })}
-        />
-        <label htmlFor="edit-email">Name</label>
-        <input
-          className="edit-input"
-          id="edit-email"
-          placeholder="marmtee@gmail.com"
-          {...register("edit-email", {
-            required: "this is required",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: "invalid Email Address",
-            },
-          })}
-        />
-        <button type="submit" className="edit-profile_submit">SUBMIT</button>
+        <label htmlFor="edit-name" className="editProfile_title">
+          Name
+        </label>
+        <div className="editProfile_input_container">
+          <input
+            type="text"
+            className="editProfile_input"
+            placeholder="mariam"
+            id="edit-name"
+            {...register("edit-name", { required: "this is required" })}
+          />
+          <img src={editNameIcon} alt="edit name icon" className="icon-start" />
+          <img src={clearEdit} alt="clear edit icon" className="icon-end" />
+
+        </div>
+        <label htmlFor="edit-email" className="editProfile_title">
+          Email
+        </label>
+        <div className="editProfile_input_container">
+          <input
+            type="text"
+            className="editProfile_input"
+            id="edit-email"
+            placeholder="marmtee@gmail.com"
+            {...register("edit-email", {
+              required: "this is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "invalid Email Address",
+              },
+            })}
+          />
+        </div>
+        <button type="submit" className="edit-profile_submit">
+          SUBMIT
+        </button>
       </form>
       <button onClick={closeProfileEdit}>CLOSE MODAL</button>
     </ReactModal>
