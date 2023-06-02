694503f8791183cd30069f29f2870be930d5a42c
Usieme Blessing <usiemeblessing@gmail.com>
2023-06-02 17:10:20 +0100

    modified files

--- src/components/Activation.jsx
+++ src/components/Activation.jsx
@@ -7,7 +7,7 @@ const Activation = () => {
       <div className="activation-container">
         <img src={Image} alt="activation" />
         <h1>activation code</h1>
-        <p>An email was sent to *****@gmail.com with an active code</p>
+        <p className='sent-email'>An email was sent to *****@gmail.com with <br></br> an active code</p>
         <div className="code-box-container">
           <div className="code-box"><p>*</p></div>
           <div className="code-box"><p>*</p></div>
@@ -15,7 +15,7 @@ const Activation = () => {
           <div className="code-box"><p>*</p></div>
         </div>
         <p>Did not receive code? <span>Resend Code</span></p>
-      <button className="verify-proceed">verify and proceed</button>
+      <button className="verify-proceed-button">verify and proceed</button>
       </div>
     </React.Fragment>
   )
