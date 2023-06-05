f82a7467371a2964d7aa7198ecff4a03859cd841
Usieme Blessing <usiemeblessing@gmail.com>
2023-06-05 18:29:11 +0100

    created cart component

--- src/components/Activation.jsx
+++ src/components/Activation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from "../images/activation.svg"
+import "../styles/activation.css";
 
 const Activation = () => {
   return (
---
+++ src/components/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/cart.css";
+
+
+const Cart = () => {
+  return (
+      <React.Fragment>
+          <div className='cart-container'>
+              
+         </div>
+   </React.Fragment>
+  )
+}
+
+export default Cart
