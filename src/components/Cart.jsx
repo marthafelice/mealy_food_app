411fb657f52cfc1d62db79b8e1c6311b9fd96e62
Usieme Blessing <usiemeblessing@gmail.com>
2023-06-08 14:46:42 +0100

    modified files

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
+import React from "react";
 import "../styles/cart.css";
-import CartPageImage from "../images/cart-page-image.png"
+import CartPageImage from "../images/cart-page-image.png";
 import { useState } from "react";
 
 const Cart = () => {
-
   const [count, setCount] = useState(0);
 
   return (
-      <React.Fragment>
-          <div className='cart-container'>
+    <React.Fragment>
+      <div className="cart-container">
         <img src={CartPageImage} alt="breakfast" />
-         <h1>full breakfast</h1>
-        <p className='food-description'>120g of Yam, potatoes, and plantain, served <br></br> with sauteed vegetables, egg sauce & sausage.</p>
-      <p className='price'>$ 3,000</p>
-      <div className='order-quantity-add-to-cart'>
-        <div className='order-quantity-container'>
-<button className="btn decrease-order" onClick={() => setCount(count - 1)}>-</button>
-  <p className='quantity'>{count}</p>
-        <button className="btn increase-order" onClick={() => setCount(count + 1)}>+</button>
+        <h1>full breakfast</h1>
+        <p className="food-description">
+          120g of Yam, potatoes, and plantain, served <br></br> with sauteed
+          vegetables, egg sauce & sausage.
+        </p>
+        <p className="price">$ 3,000</p>
+        <div className="order-quantity-add-to-cart-container">
+          <div className="order-quantity-container">
+            <button
+              className="btn decrease-order"
+              onClick={() => setCount(count - 1)}
+            >
+              -
+            </button>
+            <p className="quantity">{count}</p>
+            <button
+              className="btn increase-order"
+              onClick={() => setCount(count + 1)}
+            >
+              +
+            </button>
+          </div>
+          <button className="add-to-cart">add to cart</button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
-            
-        </div>
-        <button className='add-to-cart'>add to cart</button>
-        </div>
-        </div>
-   </React.Fragment>
-  )
-}
-
-export default Cart
+export default Cart;
