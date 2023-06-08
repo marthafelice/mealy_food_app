c65a7e1a9b6371202290ec6c5fa1c92e97a28c96
Usieme Blessing <usiemeblessing@gmail.com>
2023-06-08 14:10:15 +0100

    modified files

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import "../styles/cart.css";
 import CartPageImage from "../images/cart-page-image.png"
-
+import { useState } from "react";
 
 const Cart = () => {
+
+  const [count, setCount] = useState(0);
+
   return (
       <React.Fragment>
           <div className='cart-container'>
         <img src={CartPageImage} alt="breakfast" />
          <h1>full breakfast</h1>
-        <p className='sent-email'>120g of Yam, potatoes, and plantain, served <br></br> with sauteed vegetables, egg sauce & sausage.</p>
+        <p className='food-description'>120g of Yam, potatoes, and plantain, served <br></br> with sauteed vegetables, egg sauce & sausage.</p>
       <p className='price'>$ 3,000</p>
-      <div className='order-qty-add-to-cart'>
-        <div className='increase-decrease-order-quantity'>
-          <p>-</p>
-          <p>1</p>
-          <p>+</p>
+      <div className='order-quantity-add-to-cart'>
+        <div className='order-quantity-container'>
+<button className="btn decrease-order" onClick={() => setCount(count - 1)}>-</button>
+  <p className='quantity'>{count}</p>
+        <button className="btn increase-order" onClick={() => setCount(count + 1)}>+</button>
+
+            
         </div>
         <button className='add-to-cart'>add to cart</button>
         </div>
