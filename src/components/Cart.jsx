b083a9f858b79f424a53827d605b742dccd75d73
Usieme Blessing <usiemeblessing@gmail.com>
2023-06-08 15:13:48 +0100

    modified files

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/cart.css";
 import CartPageImage from "../images/cart-page-image.png";
+import CloseWindowIcon from "../images/close-window.svg";
 import { useState } from "react";
 
 const Cart = () => {
@@ -9,7 +10,8 @@ const Cart = () => {
   return (
     <React.Fragment>
       <div className="cart-container">
-        <img src={CartPageImage} alt="breakfast" />
+        <img src={CloseWindowIcon} alt="close-window-icon" className="close-window-icon"/>
+        <img src={CartPageImage} alt="breakfast" className="cart-page-image" />
         <h1>full breakfast</h1>
         <p className="food-description">
           120g of Yam, potatoes, and plantain, served <br></br> with sauteed
