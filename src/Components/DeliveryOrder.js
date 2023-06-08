a42e12f192191dca2d1e377ed46f88b3a0e01b73
unknown <omobowalerohma1998@gmail.com>
2023-06-08 19:33:32 +0100

    added general class to deliveryOrder

--- src/Components/DeliveryOrder.js
+++ src/Components/DeliveryOrder.js
@@ -6,7 +6,7 @@ function DeliveryOrder() {
   return (
     <div className="order-category_delivery order-category">
       <h3>African Restaurants</h3>
-      <div className="restaurant-cards">
+      <div className="order-category_restaurant-cards">
         <RestaurantCard name="Breakfast Hub" rating="4.6" price="400" />
         <RestaurantCard name="Breakfast Hub" rating="4.6" price="400" />
         <RestaurantCard name="Breakfast Hub" rating="4.6" price="400" />
@@ -18,7 +18,6 @@ function DeliveryOrder() {
         <RestaurantCard name="Breakfast Hub" rating="4.6" price="400" />
         <RestaurantCard name="Breakfast Hub" rating="4.6" price="400" />
         <RestaurantCard name="Breakfast Hub" rating="4.6" price="400" />
-      
       </div>
     </div>
   );
