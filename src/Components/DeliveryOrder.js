3d0ce3d256233fedec876410d9d25ff7bbe5f70d
unknown <omobowalerohma1998@gmail.com>
2023-06-08 08:13:15 +0100

    created the card componnet

--- src/Components/DeliveryOrder.js
+++ src/Components/DeliveryOrder.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "../styles/categories.css"
+import RestaurantCard from './RestaurantCard'
 
 function DeliveryOrder() {
   return (
-    <div className='order-category_delivery'>
-      Deliveryccccccccccccccccc
+    <div className='order-category_delivery order-category'>
+      <h3>African Restaurants</h3>
+     <RestaurantCard name="Breakfast Hub" rating="4.6" price="400"/>
     </div>
   )
 }
---
+++ src/Components/RestaurantCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RestaurantCard({name,rating,price}) {
+  return (
+    <div>
+     Restaurant card 
+    </div>
+  )
+}
+
+export default RestaurantCard
