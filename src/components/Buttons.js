7d8a67785eb6298a418fb179af139f8d2ce6d913
unknown <omobowalerohma1998@gmail.com>
2023-06-09 14:10:26 +0100

    nothing

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/buttons.css";
-import { getByText } from "@testing-library/react";
+// import { getByText } from "@testing-library/rea
 
 function ButtonLarge({ text,classname }) {
   return (
