dc76b1387b21220b17d0f038a831636f32ff99c0
ChinonyeRuth <ruthibeakanma@gmail.com>
2023-06-09 04:49:39 +0100

    Finished Landing Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import DeliveryAddress from './Components/DeliveryAddress'
+import LandingPage from './Components/LandingPage'
 import ForgotPassword from './Pages/ForgotPassword'
 function App() {
   return (
     <div className="App">
       <ForgotPassword />
       <DeliveryAddress />
+      <LandingPage />
     </div>
   )
 }
