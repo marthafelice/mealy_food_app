9f93a5a66e053debc88847e7d57539f19b21c2b5
unknown <omobowalerohma1998@gmail.com>
2023-06-12 10:36:17 +0100

    removed deliveryOrder comp

--- src/Components/OrderCategory.js
+++ src/Components/OrderCategory.js
@@ -2,18 +2,18 @@ import React, { useEffect } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 
 function OrderCategory() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    navigate("/home/deliveryOrder"); // Redirect to the "deliveryOrder" page when component mounts
-  }, [navigate]);
+  // useEffect(() => {
+  //   navigate("/home/deliveryOrder"); // Redirect to the "deliveryOrder" page when component mounts
+  // }, [navigate]);
 
   return (
     <section className="categories-section_order">
       <div className="order-types">
-        <NavLink to="deliveryOrder" className="order-type" end>
+        {/* <NavLink to="deliveryOrder" className="order-type" end>
           Delivery
-        </NavLink>
+        </NavLink> */}
         <NavLink to="pickupOrder" className="order-type">
           Pickup
         </NavLink>
