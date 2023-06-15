9991fa3061902a271274a1674ebcfa9fc413f64f
unknown <omobowalerohma1998@gmail.com>
2023-06-15 09:38:51 +0100

    added hover effect to card

--- src/Components/MealCategory.js
+++ src/Components/MealCategory.js
@@ -4,7 +4,7 @@ import ChineseCategory from "../images/chinesefood.svg";
 
 function MealCategory({categoryName}) {
   return (
-    <div className="category-meal_img-container">
+    <div className="category-meal-container">
       <img src={ChineseCategory} alt="category-icon" />
       <p className="category-meal_name">{categoryName}</p>
     </div>
