d53717fd3d3ce4de5497aa0ffd80e62630acbc5c
afolaode <afolaode@gmail.com>
2023-06-21 23:16:14 +0100

    updated cartModal,restuarantcard, livechat and  consumed api for sign up

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -15,47 +15,50 @@ const CartModal = ({ isCartModalOpen }) => {
     setDisplayCart(false);
   }
   return (
-    <ReactModal
-      isOpen={isCartModalOpen}
-      onRequestClose={closeCartModal}
-      className='cart-window'
-    >
-      <div className='cart-modal-content-container'>
-        <img
-          src={CloseWindowIcon}
-          alt='close-window-icon'
-          className='close-icon_cart'
-          onClick={closeCartModal}
-        />
-        <img src={CartPageImage} alt='breakfast' className='cart-modal-img' />
-        <div className='cart-modal-content'>
-          <h3>Full Breakfast</h3>
-          <p className='food-description'>
-            120g of Yam, potatoes, and plantain, served with sauteed vegetables,
-            egg sauce & sausage.
-          </p>
-          <p className='price'>$ 3,000</p>
-          <div className='order-quantity-add-to-cart-container'>
-            <div className='order-quantity-container'>
-              <button
-                className='btn decrease-order'
-                onClick={() => setCount(count - 1)}
-              >
-                -
-              </button>
-              <p className='quantity'>{count}</p>
-              <button
-                className='btn increase-order'
-                onClick={() => setCount(count + 1)}
-              >
-                +
-              </button>
+    <>
+      <ReactModal
+        isOpen={isCartModalOpen}
+        onRequestClose={closeCartModal}
+        overlayClassName='overlay-cart'
+         className="cart-modal"
+      >
+        <div className='cart-window cart-modal-content-container'>
+          <img
+            src={CloseWindowIcon}
+            alt='close-window-icon'
+            className='close-icon_cart'
+            // onClick={closeCartModal}
+          />
+          <img src={CartPageImage} alt='breakfast' className='cart-modal-img' />
+          <div className='cart-modal-content'>
+            <h3>Full Breakfast</h3>
+            <p className='food-description'>
+              120g of Yam, potatoes, and plantain, served with sauteed
+              vegetables, egg sauce & sausage.
+            </p>
+            <p className='price'>$ 3,000</p>
+            <div className='order-quantity-add-to-cart-container'>
+              <div className='order-quantity-container'>
+                <button
+                  className='btn decrease-order'
+                  onClick={() => setCount(count - 1)}
+                >
+                  -
+                </button>
+                <p className='quantity'>{count}</p>
+                <button
+                  className='btn increase-order'
+                  onClick={() => setCount(count + 1)}
+                >
+                  +
+                </button>
+              </div>
+              <button className='add-to-cart'>add to cart</button>
             </div>
-            <button className='add-to-cart'>add to cart</button>
           </div>
         </div>
-      </div>
-    </ReactModal>
+      </ReactModal>
+    </>
   );
 };
 
